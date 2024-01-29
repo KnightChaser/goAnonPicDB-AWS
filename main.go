c0e65d604dde82cf07812ea868f40e81a9bb34f3
Lee Garam <agerio100@naver.com>
2024-01-29 11:41:38 +0900

    Seperate functions

--- main.go
+++ main.go
@@ -18,11 +18,15 @@ func init() {
 	}
 }
 
-func main() {
+func setupAWSClient() s3.S3 {
 	// Set up AWS S3 client
 	awsConfig := aws.NewConfig().WithRegion(os.Getenv("AWS_REGION"))
 	awsSession := session.Must(session.NewSession(awsConfig))
 	s3Client := s3.New(awsSession)
+	return *s3Client
+}
+
+func main() {
 
 	// Set up Gin router
 	router := gin.Default()
@@ -31,35 +35,7 @@ func main() {
 	router.LoadHTMLGlob("static/*")
 
 	// Route to upload image
-	router.POST("/upload", func(c *gin.Context) {
-		file, err := c.FormFile("image")
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Image not provided"})
-			return
-		}
-
-		// Open the file
-		uploadedFile, err := file.Open()
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error opening file"})
-			return
-		}
-		defer uploadedFile.Close()
-
-		// Upload file to S3
-		_, err = s3Client.PutObject(&s3.PutObjectInput{
-			Bucket: aws.String(os.Getenv("S3_BUCKET_NAME")),
-			Key:    aws.String(file.Filename),
-			Body:   uploadedFile,
-		})
-
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error uploading file to S3"})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{"message": "Image uploaded successfully"})
-	})
+	router.POST("/upload", serverImageUploadHandler)
 
 	// Route to serve the HTML file
 	router.GET("/", func(c *gin.Context) {
@@ -68,4 +44,38 @@ func main() {
 
 	// Run the server
 	router.Run(":8080")
+}
+
+// upload an image from the webserver
+func serverImageUploadHandler(context *gin.Context) {
+
+	s3Client := setupAWSClient()
+
+	file, err := context.FormFile("image")
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Image not provided"})
+		return
+	}
+
+	// Open the file
+	uploadedFile, err := file.Open()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Error opening file"})
+		return
+	}
+	defer uploadedFile.Close()
+
+	// Upload file to S3
+	_, err = s3Client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(os.Getenv("S3_BUCKET_NAME")),
+		Key:    aws.String(file.Filename),
+		Body:   uploadedFile,
+	})
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Error uploading file to S3"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Image uploaded successfully"})
 }
